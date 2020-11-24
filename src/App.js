207c393889bc6fcdd26ce78756ed6dd77270b4ba
swardbladpaulo <paulo@swardblads.se>
2020-11-24 21:52:32 +0100

    add semantic ui, import to index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import { Button, Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        This is my RPS App
-      </div>
+      <Container id="playcontainer">
+        <div style={{ fontSize: 20, fontWeight: "bold", color: "blue" }}>This is my RPS App</div>
+        <div style={{ fontSize: 65 }}> ✊ ✋ ✌️ </div>
+      </Container>
+         <Container id="buttoncontainer">
+              <Button id="playbutton">Play</Button>
+          </Container>
     )
   }
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'purple',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }
+// })
 
 export default App;
