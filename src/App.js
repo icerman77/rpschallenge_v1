db0ce088b14e7fd5479eb116c4da1e697724f66d
swardbladpaulo <paulo@swardblads.se>
2020-11-24 20:57:34 +0100

    config cypress.json, delete app.test, add text to app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This is my RPS App
+      </div>
+    )
+  }
+}
+
+export default App;
